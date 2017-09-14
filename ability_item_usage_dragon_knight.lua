ddee1b9f270f52a47e13a1366bd08210d10253ce
zs7779 <zs7779@hotmail.com>
2017-09-14 16:29:41 -0400

    Update ability_item_usage_dragon_knight.lua

--- ability_item_usage_dragon_knight.lua
+++ ability_item_usage_dragon_knight.lua
@@ -1,7 +1,7 @@
 ability_item_usage_generic = dofile( GetScriptDirectory().."/ability_item_usage_generic" )
 
 function GetAbilityGuide(I)
-	abilities, talents = I:GetAbilities();
+	local abilities, talents = I:GetAbilities();
 
 	local abilityLevelUp = {};
 	abilityLevelUp[1] = abilities[1];
@@ -43,19 +43,51 @@ function AbilityUsageThink()
 	
 	local BreatheFire = I:GetAbilityByName(abilities[1]);
 	local DragonTail = I:GetAbilityByName(abilities[2]);
+	local ElderDragonForm = I:GetAbilityByName(abilities[4]);
+	
+	local ElderDragonFormDesire = ConsiderElderDragonForm(I, ElderDragonForm);
 	local BreatheFireDesire, BreatheFireLoc = ConsiderPointNuke(I, BreatheFire);
 	local DragonTailDesire, DragonTailTarget = ConsiderUnitStun(I, DragonTail);
 
-	if BreatheFireDesire > 0 then
-		I:Action_UseAbilityOnLocation(BreatheFire,BreatheFireLoc);
+	if ElderDragonFormDesire > 0 then
+		I:Action_UseAbility(ElderDragonForm);
 	end
 	if DragonTailDesire > 0 then
 		I:Action_UseAbilityOnEntity(DragonTailDesire, DragonTailTarget);
 	end
+	if BreatheFireDesire > 0 then
+		I:Action_UseAbilityOnLocation(BreatheFire, BreatheFireLoc);
+	end
+	
 end
 
+function ConsiderBreatheFire(I, ability)
+	return ConsiderPointNuke(I, ability, ability:GetSpecialValueInt("end_radius"), ability:GetCastPoint());
+end
+function COnsiderDragonTail(I, ability)
+	return ConsiderUnitStun(I, ability, 0);
+end
 
 function ConsiderElderDragonForm(I, ability)
+	if not ability:IsFullyCastable() then
+		return BOT_ACTION_DESIRE_NONE;
+	end
+	local activeMode = I:GetActiveMode();
+
 	-- Something something like if pushing and close to tower, of if attack and close to target
+	if activeMode >= BOT_MODE_PUSH_TOWER_TOP and 
+		activeMode <= BOT_MODE_PUSH_TOWER_BOT then
+		local towers = GetNearbyTowers(500, true);
+		if #towers > 0 then return BOT_ACTION_DESIRE_MODERATE; end
+	end
+	
+	if activeMode == BOT_MODE_ROAM or
+		 activeMode == BOT_MODE_TEAM_ROAM or
+		 activeMode == BOT_MODE_DEFEND_ALLY or
+		 activeMode == BOT_MODE_ATTACK then
+		 local target = I:GetTarget();
+		 if GetUnitToUnitDistance(I, target) <= 500 and not I:IsLow() then return BOT_ACTION_DESIRE_MODERATE; end
+	 end
+	
 	return BOT_ACTION_DESIRE_NONE;
 end
